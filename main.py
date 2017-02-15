8766cd2658af28df3a7766d9257e77f06943b464
abbachinomnso <abbachinomnso@gmail.com>
2017-02-15 03:31:15 +0000

    final stuff i guess… does some data processing

--- main.py
+++ main.py
@@ -17,44 +17,52 @@ def do_connect():
             pass
     print('network config:', sta_if.ifconfig())
 
+def mqqt_connect():
+    client = MQTTClient('unnamed1', '192.168.0.10')
+    client.connect()
+
+def define_pins_addresses():
+    i2c = I2C(scl = Pin(4),sda = Pin(5),freq = 500000) #define i2c pins
+    addr = i2c.scan()[0] #Finding the address of the device
+    #Setting the registers to variables
+    CTRL_Reg1 = 0x20
+    OUT_RegX_L = 0x28
+    OUT_RegY_L = 0x2A
+    OUT_RegZ_L = 0x2C
+    OUT_RegX_H = 0x29
+    OUT_RegY_H = 0x2B
+    OUT_RegZ_H = 0x2D
+    CTRL_Reg4 = 0x23
+    #Setting certain bits to 0 and certain bits to 1 depending on our needs
+    #I2C.writeto_mem(addr, memaddr, buf)
+    i2c.writeto_mem(addr, CTRL_Reg1, bytearray([23]))
+    i2c.writeto_mem(addr, CTRL_Reg4, bytearray([18])) #resolution 4G and high resolution output
+
 def convert_to_ms (high,low):
     a = bytearray(high)
     b = bytearray(low)
     aa = b + a
-    Gval = unpack('<h', aa)[0] #signed integer
-    return (str((Gval*39.2)/32768));
+    Gval = unpack('<h', aa)[0] #signed integer of G value on a scale up to 32768 being 4G
+    return ((Gval*39.2)/32768);
 
-do_connect() #connect to the internet /wifi network
-client = MQTTClient('unnamed1', '192.168.0.10')
-client.connect()
+def check_safe (current_val , prev_val):
+    diff_ = current_val - prev_val
+    if abs(diff_) > 10 : #collision occured
+        verdict = False
+    else:
+        verdict = True
 
-i2c = I2C(scl = Pin(4),sda = Pin(5),freq = 500000)
+    return verdict;
 
-addr = i2c.scan()[0] #Finding the address of the device
 
-#Setting the registers to variables
-#vigram you need to say why you setting the registers to variables
-CTRL_Reg1 = 0x20
-OUT_RegX_L = 0x28
-OUT_RegY_L = 0x2A
-OUT_RegZ_L = 0x2C
-OUT_RegX_H = 0x29
-OUT_RegY_H = 0x2B
-OUT_RegZ_H = 0x2D
-CTRL_Reg4 = 0x23 #this sets it to +-16G resolution and high resolution output
-
-#Setting certain bits to 0 and certain bits to 1 depending on our needs
-#I2C.writeto_mem(addr, memaddr, buf)
-i2c.writeto_mem(addr, CTRL_Reg1, bytearray([23]))
-i2c.writeto_mem(addr, CTRL_Reg4, bytearray([18])) #resolution 4G and high res output
-#Reading the data
-#I2C.readfrom_mem(addr, memaddr, nbytes)
-alpha = i2c.readfrom_mem(addr,CTRL_Reg1,1)
+xval = 0
+yval = 0
+zval = 0
 ms = "m/s^2"
 
-xval=0
-yval=0
-zval=0
+do_connect() #connect to the internet /wifi network
+mqqt_connect() #connect to mqqt connect
+define_pins_addresses() #define pins of i2c and give names to addresses we need
 
 while True:
     x_h = i2c.readfrom_mem(addr,OUT_RegX_H,1)
@@ -74,27 +82,23 @@ while True:
     yval = convert_to_ms(y_h , y_l)
     zval = convert_to_ms(z_h , z_l)
 
-    if(xval<a):
-        print ("Collision")
+    #check which values are safe
+    check_x = check_safe (xval,a)
+    check_y = check_safe (yval,b)
+    check_z = check_safe (zval,c)
+    #give verdict
+    if (check_x = True or check_y = True or check_z = True) :
+        verdict = "safe"
+    else:
+        verdict = "unsafe - possible collision"
+
+    #need values as string
+    xvall = str(xval)
+    yvall = str(yval)
+    zvall = str(zval)
 
 
-    if(yval<b):
-        print ("Collision")
-
-
-    #Reading and printing the rest of the data
-
-    #print(i2c.readfrom_mem(addr,OUT_RegX_H,1),i2c.readfrom_mem(addr,OUT_RegX_L,1))
-    #print (xval,ms )
-    #print (yval,ms )
-    #print (zval,ms )
-
-    payload = ujson.dumps({"xacc": (xval + ms) , "yacc": (yval + ms) , "zacc": (zval + ms)})
+    payload = ujson.dumps({"xacc": (xvall + ms) , "yacc": (yvall + ms) , "zacc": (zvall + ms), "verdict": verdict})
     print (payload)
     client.publish('esys/<fantastic four>/...', bytearray(str(payload)))
-    time.sleep(1.0)  # Delay for 3 seconds
-
-
-#client = MQTTClient('unnamed1', '192.168.0.10')
-#client.connect()
-#client.publish('esys/<fantastic four>/...', bytearray(str(beta)))
+    time.sleep(0.5)  # Delay for 0.5 seconds
